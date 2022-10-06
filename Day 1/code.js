bf8151c9b563309a4df56f11aac33aea4c8d60b8
AryanKansagara <108116767+AryanKansagara@users.noreply.github.com>
2022-10-06 21:44:24 +0530

    day1 solution added

--- Day 1/code.js
+++ Day 1/code.js
@@ -1,1 +1,26 @@
-//Write Your Code Here
+// Problem can be found here : https://www.hackerrank.com/challenges/js10-arithmetic-operators/problem
+
+/**
+Calculate the area and perimeter of a rectangle.
+
+   length: The length of the rectangle.
+   width: The width of the rectangle.
+
+   Return a number denoting the rectangle's area and perimeter.
+*/
+
+
+function getArea(length, width) {
+    let area;
+    // Code
+    area = length*width;
+    return area;
+}
+
+
+function getPerimeter(length, width) {
+    let perimeter;
+    // Code
+    perimeter = 2*(length+width);
+    return perimeter;
+}
