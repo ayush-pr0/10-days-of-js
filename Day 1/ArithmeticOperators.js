c46fbc9300f48341670aab7fd2feecd5e34da9a2
Akshay Kumar Maurya <akshay21sep@gmail.com>
2022-10-07 14:27:49 +0530

    day1 solution completed

---
+++ Day 1/ArithmeticOperators.js
@@ -0,0 +1,15 @@
+
+getArea(20, 30);
+getPerimeter(40, 90);
+
+function getArea(length, width) {
+    let area;
+    area = length * width       
+    return area;
+}
+
+function getPerimeter(length, width) {
+    let perimeter;
+    perimeter= 2 * (length+width)
+    return perimeter;
+}
