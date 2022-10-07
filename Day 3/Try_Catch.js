f4cecea23a64e776b5996e0865c4b5e5b1dd0d50
Protyay Mondal <protyaymondal32@gmail.com>
2022-10-07 15:56:30 +0530

    Removed Comment and changed Try_catch.js

--- Day 3/Try_Catch.js
+++ Day 3/Try_Catch.js
@@ -1,38 +1,10 @@
-'use strict';
 
-/*process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
-    });
-    
-    main();    
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-*/
-
-/*
- * Complete the reverseString function
- * Use console.log() to print to stdout.
- */
 function reverseString(s) {
     try {
-        const rev = s.split('').reverse().join('')
-        console.log(rev);      
-    } catch (error) {
-        console.log(error.message);
+        s = s.split("").reverse().join("");
+    } catch (err) {
+        console.log(err.message);
+    } finally {
         console.log(s);
-    } 
+    }
 }
