eb1c2a3af4a23b83a938845b0d5b9278c8d1b3a8
Mohan Agrawal <105565637+gituser9911@users.noreply.github.com>
2023-10-29 23:26:22 +0530

    Update ArithmeticOperators.js
    code updated

--- Day 1/ArithmeticOperators.js
+++ Day 1/ArithmeticOperators.js
@@ -1,15 +1,23 @@
-
-getArea(20, 30);
-getPerimeter(40, 90);
-
+// Calculate and return the area of a rectangle
 function getArea(length, width) {
-    let area;
-    area = length * width       
+    const area = length * width;
     return area;
 }
 
+// Calculate and return the perimeter of a rectangle
 function getPerimeter(length, width) {
-    let perimeter;
-    perimeter= 2 * (length+width)
+    const perimeter = 2 * (length + width);
     return perimeter;
 }
+
+// Example usage:
+const area1 = getArea(20, 30);
+const perimeter1 = getPerimeter(20, 30);
+
+const area2 = getArea(40, 90);
+const perimeter2 = getPerimeter(40, 90);
+
+console.log("Area 1:", area1);
+console.log("Perimeter 1:", perimeter1);
+console.log("Area 2:", area2);
+console.log("Perimeter 2:", perimeter2);
