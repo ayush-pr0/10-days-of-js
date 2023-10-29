a5905a23914939708a4435f8c12b6e29f05c9c1f
Mohan Agrawal <105565637+gituser9911@users.noreply.github.com>
2023-10-29 21:29:50 +0530

    Update Conditional Statements_If-Else.js
    Added proper comments with clean code.

--- Day 2/Conditional Statements_If-Else.js
+++ Day 2/Conditional Statements_If-Else.js
@@ -1,13 +1,19 @@
 function getGrade(score) {
-    let grade;
-    // Write your code here
-    const scores = [0, 5, 10, 15, 20, 25, 30];
-    const grades = ['F','E','D','C','B','A'];
-    
-    for(let i = 0; i < scores.length; i++){
-        if(score<=scores[i+1] && score>scores[i]){
-            grade = grades[i]
+    // Define the score boundaries and corresponding grades
+    const scoreBoundaries = [0, 5, 10, 15, 20, 25, 30];
+    const grades = ['F', 'E', 'D', 'C', 'B', 'A'];
+
+    // Initialize the grade to 'F' by default
+    let grade = 'F';
+
+    // Iterate through the score boundaries
+    for (let i = 0; i < scoreBoundaries.length; i++) {
+        // Check if the score falls within the current range
+        if (score > scoreBoundaries[i] && score <= scoreBoundaries[i + 1]) {
+            grade = grades[i];
+            break; // No need to continue the loop once the grade is found
         }
     }
+
     return grade;
 }
