8872410440a2e7b654404cae279675462fdca094
Ayush Khandelwal <61639552+ayush-geeks@users.noreply.github.com>
2023-10-30 18:16:33 +0530

    Update Arrays.js
    added code with comment

--- Day 3/Arrays.js
+++ Day 3/Arrays.js
@@ -1,27 +1,21 @@
-
 function getSecondLargest(nums) {
-    // Complete the function
-   let first = nums[0]; 
-   let second = -1;
+    let first = nums[0]; // Initialize 'first' with the first element
+    let second = -1;    // Initialize 'second' with a negative value
+    
     for (let i = 0; i < nums.length; i++) {
-    if (nums[i] > first) {
-        second = first;
-        first = nums[i];
+        if (nums[i] > first) {
+            second = first; // Update 'second' with the previous 'first'
+            first = nums[i]; // Update 'first' with the new maximum
+        } else if (nums[i] > second && nums[i] < first) {
+            second = nums[i]; // Update 'second' with a new value if it's greater than the current 'second' but smaller than 'first'
+        }
     }
-
-    if (nums[i] > second && nums[i] < first) {
-        second = nums[i];
-    }
+    
+    return second; // Return the second largest value
 }
-
-
-return second;
-}
-
 
 function main() {
-    const n = +(readLine());
-    const nums = readLine().split(' ').map(Number);
-    
-    console.log(getSecondLargest(nums));
+    const n = +(readLine()); // Read the number of elements
+    const nums = readLine().split(' ').map(Number); // Read and split the array
+    console.log(getSecondLargest(nums)); // Call the function and print the result
 }
